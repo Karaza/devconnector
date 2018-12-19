29b81ed5f5769cbe238799b8c7a6bed43e40924e
Sophie Kowalski <skowalski57@gmail.com>
2018-12-19 21:25:19 +0100

    prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://kara:azerty123@ds113136.mlab.com:13136/devconnector',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
